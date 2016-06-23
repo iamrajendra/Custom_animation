d509cc78bc71a6b178eed7f33f452418cb0ceda0
rajendra.softinator <rajendra@softinator.com>
2016-06-23 06:00:07 -0700

    tocuh feedback animation, circular reveal

---
+++ app/src/main/java/com/example/admin/customanimationexample/MainActivity.java
@@ -0,0 +1,149 @@
+package com.example.admin.customanimationexample;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewAnimationUtils;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+/**
+ *
+ */
+
+public class MainActivity extends AppCompatActivity  {
+    private Switch button;
+    private View view;
+    private String Tag="MainActivity";
+
+    /**
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        init();
+    }
+
+    /**
+     *
+     */
+    private void init() {
+        button = (Switch) findViewById(R.id.button);
+        button.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                 if(isChecked)
+                 {
+                     show();
+                 }else
+                 {
+                     hide();
+                 }
+
+            }
+        });
+        view = (View) findViewById(R.id.view);
+    }
+
+    private void show() {
+        int cx = view.getWidth() / 2;
+        int cy = view.getHeight() / 2;
+
+// get the final radius for the clipping circle
+        float finalRadius = (float) Math.hypot(cx, cy);
+
+// create the animator for this view (the start radius is zero)
+        Animator anim =
+                null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            anim = ViewAnimationUtils.createCircularReveal(view, cx, cy, 0, finalRadius);
+        }else
+        {
+            Log.e(Tag,"This feature is not available below v21");
+        }
+
+        view.setVisibility(View.VISIBLE);
+        anim.start();
+
+    }
+
+    private void hide() {
+        // get the center for the clipping circle
+        int cx = view.getWidth() / 2;
+        int cy = view.getHeight() / 2;
+
+// get the final radius for the clipping circle
+        float finalRadius = (float) Math.hypot(cx, cy);
+
+// create the animator for this view (the start radius is zero)
+        Animator anim =
+                null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            anim = ViewAnimationUtils.createCircularReveal(view, cx, cy, 0, finalRadius);
+
+            // make the view invisible when the animation is done
+            anim.addListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    super.onAnimationEnd(animation);
+                    view.setVisibility(View.INVISIBLE);
+                }
+            });
+
+        }else
+        {
+            Log.e(Tag,"This feature is not available below v21");
+        }
+
+        anim.start();
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
